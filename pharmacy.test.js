b8a5aa4c24c09029f982bc95665f1cbfdf8f0238
molka_jelassi <molka.djelassi@gmail.com>
2025-11-14 19:56:48 +0100

    add new jest unit test

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -3,7 +3,15 @@ import { Drug, Pharmacy } from "./pharmacy";
 describe("Pharmacy", () => {
   it("should decrease the benefit and expiresIn", () => {
     expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)],
+      [new Drug("test", 1, 2)]
     );
+  });
+
+  it("should decrease Dafalgan benefits twice as fast as normal drugs", () => {
+    const drug = new Drug("Dafalgan", 10, 40);
+    const pharmacy = new Pharmacy([drug]);
+    pharmacy.updateBenefitValue();
+    expect(pharmacy.drugs[0].benefit).toBe(38);
+    expect(pharmacy.drugs[0].expiresIn).toBe(9);
   });
 });
